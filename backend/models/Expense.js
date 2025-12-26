ccdffc4cca0e9b822da311c7828c06189adfd6a9
Thompson Daniel <thompsonacedaniel@gmail.com>
2025-12-26 02:38:28 +0100

    All 4 Christmas builds UI and config done. Controllers and js logic here I come. Good night and Happy Boxing Day

--- backend/models/Expense.js
+++ backend/models/Expense.js
@@ -3,9 +3,12 @@ import mongoose from "mongoose";
 const expenseSchema = mongoose.Schema({
   date: { type: Date, required: true },
   income: { type: Number, required: true },
+  incomeNote: { type: String, trim: true },
   expenses: {
     item: { type: String, required: true, trim: true },
     value: { type: Number, required: true },
+    time: { type: Date, required: true },
+    category: { type: String, required: true, trim: true },
   },
 });
 
